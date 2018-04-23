a4ea2183bda9622ced76016ba31838294c8abdd6
Anler Hernández Peral <inbox@anler.me>
2018-04-23 11:30:21 +0200

    wip2

--- src/implicits/core.clj
+++ src/implicits/core.clj
@@ -1,37 +1,27 @@
 (ns implicits.core
   (:refer-clojure :exclude [defn]))
 
-(defonce ^:private ^:dynamic *registry* (atom {}))
-
-(def implicits
-  "Returns a map of currently-defined implicits."
-  (fn []
-    @*registry*))
-
-(defn- normalize [sym]
-  (symbol (or (namespace sym)
-              (str *ns*))
-          (name sym)))
-
 (defmacro defimplicit
-  "Define an implicit value named by the provided symbol."
-  [sym & [docstring init]]
-  (let [[docstring value] (if (and (string? docstring)
-                                   (< (count &form) 4))
-                            ["" docstring]
-                            [docstring init])]
-    (swap! *registry* assoc (normalize sym) value)
-    `(def ~(with-meta sym
-             {:doc (str docstring "\n" "Implicit value.")
-              ::implicit true})
-       ~value)))
+  "Like def, but defines an implicit value."
+  [sym & rest]
+  `(def ~(with-meta sym {:dynamic true, :implicit true})
+     ~@rest))
 
-(defn- with-implicit-params
+(defn- with-implicits
   [implicits f]
-  (fn [& args]
-    (with-bindings* implicits
-      (fn []
-        (apply f args)))))
+  (fn [& args]    
+    (with-bindings* (if (and (coll? (first args))
+                             (= 'implicitly (first (first args))))
+                      (merge implicits (into {} (map (fn [[sym val]]
+                                                       [(resolve sym) val])
+                                                     (partition 2 (rest (first args))))))
+                      implicits)
+      (fn [args]
+        (apply f args))
+      (if (and (coll? (first args))
+               (= 'implicitly (first (first args))))
+        (rest args)
+        args))))
 
 (defmacro defn
   "Similar to clojure.core/defn but it uses two parameter lists where
@@ -40,7 +30,7 @@
   bound to the second parameter list, if the first parameter though is
   of the form (implicitly param...), those params will be bound to the
   first parameter (implicit parameters) list."
-  [& defn-args]
+  [& fdecl]
   (let [[fname & fdecl]         fdecl
         [docstring fdecl]       (if (string? (first fdecl))
                                   [(first fdecl) (rest fdecl)]
@@ -49,39 +39,29 @@
                                   [(first fdecl) (rest fdecl)]
                                   [{} fdecl])
         [implicit-params fdecl] [(first fdecl) (rest fdecl)]
-        evidences               (implicits)
-        implicit-param-values   (reduce conj (map #(if (contains? evidences %)
-                                                     [(-> % name symbol) (% evidences)]
-                                                     (throw (ex-info "Implicit param not found."
-                                                                     {:cause :implicit-param-not-found
-                                                                      :param %})))
-                                                  implicit-params))]
-    ;; {:arglists (map #(cons '(implicitly implicit-params)?))}
-    `(with-implicits
-       ~implicit-params-values
-       (fn ~(with-meta fname
+        implicits#              (into {} (map #(if-let [v (resolve %)]
+                                                 [v (deref v)]
+                                                 (throw (ex-info "Implicit param not found."
+                                                                 {:cause :implicit-param-not-found
+                                                                  :param %})))
+                                              implicit-params))]
+    `(def ~(with-meta fname
              (assoc attr-map
-                    :doc (str docstring "\n Implicits: " implicit-params)))
-         ~@fdecl))))
+                    :doc (str docstring "\n Implicits: " implicit-params)
+                    ;; TODO: arglists metadata
+                    ))
+       (with-implicits
+         ~implicits#
+         (fn ~@fdecl)))))
 
 (comment
-  (defn hello [::x] []
-    x)
-  (hello (list implicitly ::x "John"))
-  (macroexpand-1 '(hello (implicitly ::x "John")))
+  (defimplicit x 3)
   (macroexpand-1
-    '(defn hello [::x] []
+    '(defn hello [x] []
        x))
-  (defn x [f & [implicits args]]
-    (if (= 'implicits (first implicits))
-      (binding ) ;; call f with implicits
-      ))
 
-  (macroexpand-1
-    '(defimplicit ::x ""))
+  (defn hello [x] []
+    x)
 
-  (do (defimplicit ::x "John")
-      (defn hello-msg [::x] []
-        (str "Hello there, " x))
-      (hello-msg))
+  (hello '(implicitly x 5))
   )
